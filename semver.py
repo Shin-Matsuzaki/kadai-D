f5de62c94560c60eeb4a84b189572f8f22884e01
matsuzaki <shin.matsu.p@gmail.com>
2019-09-25 17:16:19 +0900

    D-2: インスタンスの文字列表現を得る特殊メソッドを実装

--- semver.py
+++ semver.py
@@ -7,10 +7,19 @@ class SemanticVersion:
     def __eq__(self, other):
         return self.__dict__ == other.__dict__
 
+    def __str__(self):
+        return f'{self.major}.{self.minor}.{self.patch}'
+
+
 def main():
+    # kadaiD-1
     py370 = SemanticVersion(major=3, minor=7, patch=0)
     print(SemanticVersion(3, 7, 0) == py370)
     print(SemanticVersion(3, 7, 1) == py370)
+
+    # kadaiD-2
+    print('3.7.0' == str(py370))
+
 
 if __name__ == '__main__':
     main()
